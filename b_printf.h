baa8249a296d1af82721c36e38242ace0eef11d5
David Mendelovits <david.mendelovits@gmail.com>
2018-09-17 18:23:20 -0700

    made a makefile again no way

--- b_printf.h
+++
@@ -1,6 +0,0 @@
-#ifndef B_PRINTF_H
-# define B_PRINTF_H
-
-int         parse(const char *format, va_list arg_list);
-int         b_printf(const char *format, ...);
-# endif
--- ft_atoi.c
+++ ft_atoi.c
@@ -16,17 +16,6 @@ int         ft_atoi(const char *str)
         i += (str[p] -'0') * div;
         div *= 10;
         p -= 1;
-        ft_putnbr_base(i, 10);
-        ft_putchar('\n');
     }
     return (i);
-}
-
-int         main(int argc, char **argv)
-{
-    if (argc == 2)
-    {
-        ft_putnbr_base(ft_atoi(argv[1]), 10);
-    }
-    return (0);
 }
